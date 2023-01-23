30a4fb797bd4b3642797a1fce5762f92d04d6475
sujitwhiteblink <102506419+sujitwhiteblink@users.noreply.github.com>
2023-01-23 13:09:03 +0530

    2nd commit

--- server.js
+++ server.js
@@ -8,110 +8,95 @@ const port = process.env.port || 3031;
 const cors = require("cors");
 app.use(cors());
 
-const {Blob, Buffer } = require("buffer");
+const fs = require("fs");
 
-const ffmpeg  = require("fluent-ffmpeg");
-// const metadata = require("fluent-ffmpeg").Metadata;
+const { Blob, Buffer } = require("buffer");
 
-const fs = require('fs');
+const ffmpeg = require("fluent-ffmpeg");
+const ffprobe = require("ffprobe");
 
-// import {fileTypeFromBuffer} from 'file-type'; 
+var pathToFfmpeg = require("ffmpeg-static");
+
+const ffprobeStatic = require("ffprobe-static");
+ffmpeg.setFfmpegPath(pathToFfmpeg);
+console.log(pathToFfmpeg);
+ffmpeg.setFfprobePath(ffprobeStatic.path);
+
+const moment = require("moment");
+
+const { exec, spawn } = require("child_process");
+
+// ffprobe('./videos/temp12.mp4', { path: ffprobeStatic.path }, function (err, info) {
+//   if (err) return done(err);
+//   console.log(info);
+// })
 
 // configure multer to handle file uploads
 const upload = multer({ dest: "./videos", storage: multer.memoryStorage() });
-// const upload = multer({
-//     storage: multer.memoryStorage()
-// });
 
-// Ffmpeg.setFfprobePath('C:\ffmpeg\ffmpeg-master-latest-win64-gpl\bin')
+videoChunks = [];
 
-console.log("ffmpeg",ffmpeg.ffprobe);
-ffmpeg.ffprobe('./temp12.mp4', function(err, metadata) {
-  if (err) {
-        console.log(err);
-      }
-  console.dir("metadata", metadata);
-});
-
-app.post("/sendMediaFeed", upload.single("mediaFeed"), async (request, response) => {
-  // const video = new Buffer([request.file]);
-
-  mediaBlob = request.file.buffer;
-  // Write the buffer to a temporary file
-  fs.readdir('./videos', (err, files) => {
-    console.log("files.length",files.length);
-  });
-  const tempFile = `./videos/temp12.mp4`;
-  fs.writeFileSync(tempFile, mediaBlob);
-
-  fs.readdir('./videos', (err, files) => {
-    console.log("files.length",files.length);
-  });
+app.get("/", (request, response)=>{
+  console.log("conection successfull with vercel");
+})
 
 
-  // writeFileSync(tempFile, mediaBlob);
+app.post(
+  "/sendMediaFeed",
+  upload.single("mediaFeed"),
+  async (request, response) => {
+    // const video = new Buffer([request.file]);
 
-  ffmpeg.ffprobe('./videos/temp12.mp4', function(err, metadata) {
-    if (err) {
-          console.log(err);
-        }
-    console.dir("metadata", metadata);
-});
+    const videoDuration = request.body.videoDuration;
+    console.log("videoDuration",videoDuration);
 
-  // const type = await  fileTypeFromBuffer(request.file.buffer);
+    mediaBlob = request.file.buffer;
+    let folderLength;
+    let tempFile;
+    let duration;
+    
 
-  // console.log("type",type);
-  
+    // Write the buffer to a temporary file
+    fs.readdir("./download", (err, files) => {
+      folderLength = files.length;
+      tempFile = `./videos/temp${folderLength + 1}.mp4`;
+      fs.writeFileSync(tempFile, mediaBlob);
 
-  // Extract the metadata
-  // console.log("ffmpeg ----", ffmpeg);
-  // ffmpeg(tempFile)
-  // .outputOptions([
-  //   '-f ffmetadata'
-  // ])
-  // .output('metadata.txt')
-  // .on('end', function (res) {
-  //   console.log('Metadata extraction finished!',res);
-  // })
-  // .on('error', function (err) {
-  //   console.log('An error occurred: ' + err);
-  // })
-  // .run();
+      ffmpeg(tempFile)
+            .setDuration(videoDuration)
+            .save(`./download/video${folderLength + 1}.mp4`)
+            .on('end', function() {
+              
+              console.log('Setting duration finished!');
+              // fs.unlinkSync(tempFile)
+              
+              response.send("video uploaded successfully");
 
-  // const blob = new Blob([mediaBlob],mediaBlob.type);
+            });  
+    
+    });
 
-  response.send("video uploaded successfully");
-
-  // ffprobe -show_format -show_streams blob
+  }
+);
 
 
-  // ffprobe.ffprobe(request.file.buffer, (err, metadata) => {
-  //   console.log("inside out");
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //   console.log("inside inn ",metadata);
-  //   // const duration = metadata.format;
-  //   // console.log(`Duration:  seconds`, duration);
-  // });
-  // const tempFile = './temp.webm';
-  // writeFileSync(tempFile, mediaBlob);
 
-});
+app.get("/download", (request, response) => {
+  let downloadFile;
+  fs.readdir(`./download`,(err, files) => {
+    downloadFile = files.length
 
-app.post("/testFile", upload.single("mediaFeed"), (request, response) => {
-  const video = request.body;
-  // const fileReader = new FileReader();
-  // fileReader.onload = function() {
-  //     const buffer = new Buffer(new Uint8Array(this.result));
-  //     console.log("buffer",buffer);
-  // };
-  // fileReader.readAsArrayBuffer(videoBlob);
-  // res.send('Video received');
-  console.log("video ", video);
-  //   console.log("sendMediaFeed", request);
-  console.log("sendMediaFeed body", request.body.mediaFeed);
-  response.send("file uploaded successfully");
+  const file = fs.readFile(`./download/video${downloadFile}.mp4`, (err, file)=>{
+    if(err){
+      response.status(404).send("Video not found")
+    }else{
+      response.status(200).send(file)
+
+    }
+  })
+
+})
+
 });
 
 app.listen(port, () => {
